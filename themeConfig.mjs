dc6a2941636ae5a2088af38baf3382714b5cb601
XeRnHe <114861072+XeRnHe@users.noreply.github.com>
2025-09-27 10:04:44 +0800

    Update themeConfig.mjs

--- themeConfig.mjs
+++ themeConfig.mjs
@@ -5,7 +5,7 @@ export const themeConfig = {
     // 站点标题
     title: "XeRnHe的赛博笔记",
     // 站点描述
-    description: "分享一些想法",
+    description: "记录一些想法",
     // 站点logo
     logo: "/images/logo/logo.webp", 
     // 站点地址
@@ -356,7 +356,7 @@ export const themeConfig = {
     // 站点简介
     hello: {
       enable: true,
-      text: "本人是一名高三生，在此分享一些学习笔记。"
+      text: "本人是一名高三生，在此记录一些学习笔记。"
       //text: "这里有关于<strong>开发</strong>相关的问题和看法，也会有一些<strong>奇技淫巧</strong>的分享，其中大部分内容会侧重于<strong>前端开发</strong>。希望你可以在这里找到对你有用的知识和教程。",
     },
     // 目录

92a86e41db7dd81cd6436a2b61488ba54afa5e62
XeRnHe <114861072+XeRnHe@users.noreply.github.com>
2025-09-28 23:34:57 +0800

    Update Footer.vue

--- .vitepress/theme/components/Footer.vue
+++ .vitepress/theme/components/Footer.vue
@@ -19,10 +19,6 @@
         <a class="theme link" href="https://github.com/imsyy/vitepress-theme-curve" target="_blank">
           <span class="name">主题</span>
         </a>
-        <a class="rss link" href="https://blog.imsyy.top/rss.xml" target="_blank">
-          <i class="iconfont icon-rss" />
-          <span class="name">订阅</span>
-        </a>
         <a
           class="cc link"
           href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans"
